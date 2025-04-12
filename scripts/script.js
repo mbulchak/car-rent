b3e2090c58a5ea32e5311fd9a7d9048e29bb6380
Mariia Bulchak <bulchakmariiast@gmail.com>
2025-04-12 10:49:12 +0300

    feat: add desktop version functionallity, add brakepoints; fix: fixing styles, fixing functionallity

--- scripts/script.js
+++ scripts/script.js
@@ -5,23 +5,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const confirmingOrder = document.querySelector(".confirming__order");
   const buttonMain = document.querySelector(".button__main--1");
 
-  const header = document.querySelector(".header");
-
   const processCar = document.querySelector(".process__car");
   const processCarBlock = document.querySelector(".process__car__info");
   const processLinesSecond = document.querySelectorAll(".process__line--2");
   const processLinesThird = document.querySelectorAll(".process__line--3");
   const processImagesAll = document.querySelectorAll(".process__image");
-  const processLinesAll = document.querySelectorAll(".process__line");
-
-  console.log("pr", processLinesAll);
 
   let count = 0;
 
   buttonMain.addEventListener("click", processCarPayment);
 
   function processCarPayment() {
-    if (window.innerWidth <= 1280) {
+    if (window.innerWidth <= 1500) {
       mainContentFirst.style.display = "none";
       processCar.style.display = "flex";
       extras.style.display = "flex";
@@ -32,8 +27,6 @@ document.addEventListener("DOMContentLoaded", () => {
       if (count === 2) {
         extras.style.display = "none";
         insurances.style.display = "flex";
-
-        // for process
 
         processLinesSecond.forEach(
           (line) => (line.style.backgroundColor = "#B68867")
@@ -51,19 +44,11 @@ document.addEventListener("DOMContentLoaded", () => {
         confirmingOrder.style.display = "flex";
         buttonMain.style.display = "none";
         processCarBlock.style.display = "none";
-        // processCar.style.marginBottom = "0";
         processCar.style.paddingBottom = "0";
-        // header.style.marginBottom = "24px";
-
-        // process
 
         processLinesThird.forEach(
           (line) => (line.style.backgroundColor = "#B68867")
         );
-
-        processImagesAll[0].src = "./images/descr-2/completed_progress.png";
-        processImagesAll[1].src = "./images/descr-2/indicator.png";
-        processImagesAll[1].classList.remove("process__image--gray");
 
         processImagesAll[1].src = "./images/descr-2/completed_progress.png";
         processImagesAll[2].src = "./images/descr-2/indicator.png";
@@ -71,4 +56,32 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     }
   }
+
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 1500) {
+      processCar.style.display = "none";
+      mainContentFirst.style.display = "block";
+
+      if (count === 2) {
+        extras.style.display = "flex";
+      }
+
+      if (count === 3) {
+        extras.style.display = "flex";
+        insurances.style.display = "flex";
+      }
+    } else if (count > 0) {
+      processCar.style.display = "flex";
+      mainContentFirst.style.display = "none";
+
+      if (count === 2) {
+        extras.style.display = "none";
+      }
+
+      if (count === 3) {
+        extras.style.display = "none";
+        insurances.style.display = "none";
+      }
+    }
+  });
 });
